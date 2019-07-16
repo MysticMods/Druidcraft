02d7c42968ee93033ac2eb92f8a4401edd351411
Mart <mcpkmart@hotmail.nl>
2019-07-16 20:01:52 +0200

    BOOM thats an upgrade

--- src/main/java/mystic/druidcraft/setup/EventManager.java
+++ src/main/java/mystic/druidcraft/setup/EventManager.java
@@ -1,17 +1,11 @@
 package mystic.druidcraft.setup;
 
-import epicsquid.mysticallib.MysticalLib;
-import epicsquid.mysticallib.event.RegisterItemFactoriesEvent;
 import mystic.druidcraft.Druidcraft;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Druidcraft.MODID)
 public class EventManager {
 
-    @SubscribeEvent
-    public static void onFactoryRegister(RegisterItemFactoriesEvent event){
-        int i = 0;
-    }
+
 
 }
--- src/main/java/mystic/druidcraft/setup/RegistryManager.java
+++ src/main/java/mystic/druidcraft/setup/RegistryManager.java
@@ -1,14 +1,8 @@
 package mystic.druidcraft.setup;
 
-import epicsquid.mysticallib.factories.ItemGenerator;
-import epicsquid.mysticallib.factories.ToolFactories;
-import epicsquid.mysticallib.material.BaseItemTier;
-import epicsquid.mysticallib.material.MaterialProperties;
 import mystic.druidcraft.Druidcraft;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
-import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,15 +18,6 @@ public class RegistryManager {
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("amber"));
-		ItemGenerator generator = new ItemGenerator();
-		generator.addAllFactories(ToolFactories.getFactories());
-		generator.execute(new MaterialProperties()
-										.setItemProps(() -> new Item.Properties().maxDamage(256).maxStackSize(1).group(Druidcraft.setup.tab))
-										.setName("bone")
-										.setDamage("SWORD", 7)
-										.setAttackSpeed("SWORD", 1.6f)
-										.setTier(() -> new BaseItemTier(256, 5.0f, 4.0f, 4, 4,
-														() -> Ingredient.fromItems(Items.IRON_INGOT)))
-						, event);
+
 	}
 }
