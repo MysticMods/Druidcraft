8979d8762226dfc109a831090f28f08802f564c4
Mart <mcpkmart@hotmail.nl>
2019-07-16 21:05:42 +0200

    Added items without any logic

--- src/main/java/mystic/druidcraft/setup/RegistryManager.java
+++ src/main/java/mystic/druidcraft/setup/RegistryManager.java
@@ -22,6 +22,23 @@ public class RegistryManager {
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("amber"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("ash"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("druid_book"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("fiery_glass"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("frosted_heart"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("ghostly_halo"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("ghostly_halo_empowered"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("growth_lantern_idle"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("growth_lantern_active"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("hemp"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("hemp_seeds"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("mistletoe"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("moonstone"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("radiant_javelin_idle"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("radiant_javelin_active"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("rockroot"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("rope"));
+		event.getRegistry().register(new Item(new Item.Properties().group(Druidcraft.setup.tab)).setRegistryName("soulfire"));
 		LibRegistry.registerMetalSetItems(new BoneMaterial(), event.getRegistry(), Druidcraft.MODID);
 	}
 }
