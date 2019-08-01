6cd641fc166ae950c4bf549b5ead561648a180cc
Mart <mcpkmart@hotmail.nl>
2019-08-01 22:55:18 +0200

    Make it not need water to breath

--- src/main/java/mystic/druidcraft/entity/DreadFishEntity.java
+++ src/main/java/mystic/druidcraft/entity/DreadFishEntity.java
@@ -1,12 +1,12 @@
 package mystic.druidcraft.entity;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.WaterMobEntity;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.world.World;
 
-public class DreadFishEntity extends WaterMobEntity {
+public class DreadFishEntity extends MobEntity {
 
-    public DreadFishEntity(EntityType<? extends WaterMobEntity> type, World world) {
+    public DreadFishEntity(EntityType<? extends MobEntity> type, World world) {
         super(type, world);
     }
 }
