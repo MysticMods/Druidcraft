937d050069f8d384df95b4ffdeffdf74e0a99f5a
Mart <mcpkmart@hotmail.nl>
2019-08-04 20:13:29 +0200

    AND SO WHY IS THE CREA TAB GONE?

--- src/main/java/mystic/druidcraft/registry/ModBlocks.java
+++ src/main/java/mystic/druidcraft/registry/ModBlocks.java
@@ -1,7 +1,11 @@
 package mystic.druidcraft.registry;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder("druidcraft")
 public class ModBlocks {
+
+    public static final Block DRYING_RACK = null;
+
 }
--- src/main/java/mystic/druidcraft/setup/ModSetup.java
+++ src/main/java/mystic/druidcraft/setup/ModSetup.java
@@ -1,20 +1,10 @@
 package mystic.druidcraft.setup;
 
-import mystic.druidcraft.Druidcraft;
-import mystic.druidcraft.registry.ModItems;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class ModSetup {
 
-	public ItemGroup tab = new ItemGroup(Druidcraft.MODID) {
-		@Override
-		public ItemStack createIcon() {
-			return new ItemStack(ModItems.AMBER);
-		}
-	};
+
 
 	public void init(FMLCommonSetupEvent event) {
 
