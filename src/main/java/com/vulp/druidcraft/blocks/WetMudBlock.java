35abdf72de19cf3f30bb8d9b425aad5efba2bb2a
Vulpie <VulpTheHorseDog@users.noreply.github.com>
2019-10-24 14:32:03 +1100

    Mud brick fixes

--- src/main/java/com/vulp/druidcraft/blocks/WetMudBlock.java
+++ src/main/java/com/vulp/druidcraft/blocks/WetMudBlock.java
@@ -17,35 +17,42 @@ import java.util.Random;
 public class WetMudBlock extends Block {
     private static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D);
     private Block block;
+    private int maxTicks = 0;
 
     public WetMudBlock(Block convertedBlock, Properties properties) {
         super(properties);
         this.block = convertedBlock;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void randomTick(BlockState state, World worldIn, BlockPos pos, Random random) {
         super.randomTick(state, worldIn, pos, random);
-        if (worldIn.rand.nextInt(4) == 0) {
+        if ((!worldIn.isRemote && worldIn.rand.nextInt(4) == 0) || maxTicks >= 5) {
             worldIn.setBlockState(pos, block.getDefaultState());
         }
+        else ++maxTicks;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
         entityIn.setMotion(entityIn.getMotion().mul(0.5D, 1.0D, 0.5D));
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public boolean allowsMovement(BlockState state, IBlockReader worldIn, BlockPos pos, PathType type) {
         return false;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public boolean canEntitySpawn(BlockState state, IBlockReader worldIn, BlockPos pos, EntityType<?> type) {
         return true;
