a1382eddef17591082eeecf97a168eab7b6d4eb8
Vulpie <VulpTheHorseDog@users.noreply.github.com>
2020-04-21 11:56:53 +1000

    Adding the travel pack

---
+++ src/main/java/com/vulp/druidcraft/items/TravelPackItem.java
@@ -0,0 +1,23 @@
+package com.vulp.druidcraft.items;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+public class TravelPackItem extends Item {
+
+    public TravelPackItem(Properties properties) {
+        super(properties);
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        playerIn.setActiveHand(handIn);
+        return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
+    }
+
+}
