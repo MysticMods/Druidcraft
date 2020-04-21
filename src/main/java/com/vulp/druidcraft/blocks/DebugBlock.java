0bb4a241a03e686efefd566994bd8469918895d9
Vulpie <VulpTheHorseDog@users.noreply.github.com>
2020-04-21 11:56:01 +1000

    Trying to get the bedroll mechanic to work

--- src/main/java/com/vulp/druidcraft/blocks/DebugBlock.java
+++ src/main/java/com/vulp/druidcraft/blocks/DebugBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -12,7 +13,11 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
+import java.util.Random;
+
 public class DebugBlock extends Block {
+
+    public PlayerEntity player;
 
     public DebugBlock(Properties properties) {
         super(properties);
@@ -20,7 +25,46 @@ public class DebugBlock extends Block {
 
     @Override
     public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        player.respawnPlayer();
-        return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
+        checkStuff(player);
+        return true;
+    }
+
+    public void checkStuff(PlayerEntity player) {
+        if (!player.world.isRemote()) {
+            CompoundNBT data = player.getPersistentData();
+            int spawnX;
+            int spawnY;
+            int spawnZ;
+            int tempX = 0;
+            int tempY = 0;
+            int tempZ = 0;
+            int holderX = 0;
+            int holderY = 0;
+            int holderZ = 0;
+            BlockPos mainSpawn = player.getBedLocation(player.dimension);
+                spawnX = mainSpawn.getX();
+                spawnY = mainSpawn.getY();
+                spawnZ = mainSpawn.getZ();
+            if (data.contains("TempSpawnX")) {
+                tempX = data.getInt("TempSpawnX");
+                tempY = data.getInt("TempSpawnY");
+                tempZ = data.getInt("TempSpawnZ");
+            }
+            if (data.contains("HolderSpawnX")) {
+                holderX = data.getInt("HolderSpawnX");
+                holderY = data.getInt("HolderSpawnY");
+                holderZ = data.getInt("HolderSpawnZ");
+            }
+            Druidcraft.LOGGER.debug("----------------");
+            Druidcraft.LOGGER.debug("Spawn = " + spawnX + "-" + spawnY + "-" + spawnZ + ".");
+            if (!(tempX == 0 && tempY == 0 && tempZ == 0)) {
+                Druidcraft.LOGGER.debug("Temp = " + tempX + "-" + tempY + "-" + tempZ + ".");
+            } else
+                Druidcraft.LOGGER.debug("Temp = null...");
+            if (!(holderX == 0 && holderY == 0 && holderZ == 0)) {
+                Druidcraft.LOGGER.debug("Holder = " + holderX + "-" + holderY + "-" + holderZ + ".");
+            } else
+                Druidcraft.LOGGER.debug("Holder = null...");
+        }
     }
 }
--- src/main/java/com/vulp/druidcraft/events/SpawnHandler.java
+++ src/main/java/com/vulp/druidcraft/events/SpawnHandler.java
@@ -2,12 +2,10 @@ package com.vulp.druidcraft.events;
 
 import com.vulp.druidcraft.Druidcraft;
 import com.vulp.druidcraft.blocks.BedrollBlock;
-import com.vulp.druidcraft.capabilities.ITempSpawnCapability;
-import com.vulp.druidcraft.capabilities.TempSpawnCapability;
-import com.vulp.druidcraft.capabilities.TempSpawnProvider;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.common.util.LazyOptional;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerSetSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,36 +14,102 @@ public class SpawnHandler {
 
     @SubscribeEvent
     public void onPlayerSetSpawn(PlayerSetSpawnEvent event) {
-        Druidcraft.LOGGER.debug("Bed");
-        PlayerEntity player = event.getPlayer();
-        BlockPos pos = event.getNewSpawn();
-        if(player != null && pos != null) {
-            if(player.getEntityWorld().getBlockState(pos).getBlock() instanceof BedrollBlock) {
-                LazyOptional<ITempSpawnCapability> lazyTempSpawn = player.getCapability(TempSpawnProvider.TEMP_SPAWN);
-                ITempSpawnCapability tempSpawn = lazyTempSpawn.orElse(new TempSpawnCapability());
-                tempSpawn.setTempSpawn(pos);
-                event.setCanceled(true);
+/*            if (!event.getPlayer().world.isRemote()) {
+            Druidcraft.LOGGER.debug("Bed");
+            PlayerEntity player = event.getPlayer();
+            BlockPos pos = event.getNewSpawn();
+            if (player.getEntityWorld().getBlockState(pos).getBlock() instanceof BedrollBlock) {
+                CompoundNBT playerData = player.getPersistentData();
+                playerData.putInt("TempSpawnX", pos.getX());
+                playerData.putInt("TempSpawnY", pos.getY());
+                playerData.putInt("TempSpawnZ", pos.getZ());
+                event.isCanceled();
+            }
+        } else {*/
+            PlayerEntity player = event.getPlayer();
+            BlockPos pos = event.getNewSpawn();
+            if (player.getEntityWorld().getBlockState(pos).getBlock() instanceof BedrollBlock) {
+                event.isCanceled();
+            }
+//        }
+    }
+
+    // Does trigger! Something is wrong. Place a bunch more debugs to see what path is being taken on respawn.
+    // The setting of spawns is triggering the above SetSpawnEvent. Look into whether that's what is breaking the code.
+/*    @SubscribeEvent
+    public void onSpawn(PlayerEvent.PlayerRespawnEvent event) {
+        World world = event.getPlayer().world;
+        if (!world.isRemote()) {
+            Druidcraft.LOGGER.debug("----------");
+            Druidcraft.LOGGER.debug("Spawn Event");
+            Druidcraft.LOGGER.debug("----------");
+            PlayerEntity player = event.getPlayer();
+            CompoundNBT playerData = player.getPersistentData();
+            BlockPos mainSpawn = player.getBedLocation(player.dimension);
+            if (playerData.contains("TempSpawnX")) {
+                Druidcraft.LOGGER.debug("Initiating...");
+                int tempX = playerData.getInt("TempSpawnX");
+                int tempY = playerData.getInt("TempSpawnY");
+                int tempZ = playerData.getInt("TempSpawnZ");
+                BlockPos tempPos = new BlockPos(tempX, tempY, tempZ);
+                if (tempPos != mainSpawn && playerData.contains("HolderSpawnX")) {
+                    int holderX = playerData.getInt("HolderSpawnX");
+                    int holderY = playerData.getInt("HolderSpawnY");
+                    int holderZ = playerData.getInt("HolderSpawnZ");
+                    BlockPos holderSpawn = new BlockPos(holderX, holderY, holderZ);
+                    if (!holderSpawn.equals(mainSpawn)) {
+                        Druidcraft.LOGGER.debug("Detected change in main spawn. Spawn holder modified.");
+                        playerData.putInt("HolderSpawnX", mainSpawn.getX());
+                        playerData.putInt("HolderSpawnY", mainSpawn.getY());
+                        playerData.putInt("HolderSpawnZ", mainSpawn.getZ());
+                    }
+                }
+                if (event.getPlayer().world.getBlockState(tempPos).getBlock() instanceof BedrollBlock) {
+                    Druidcraft.LOGGER.debug("Detected existing bedroll.");
+                    // Spawn holder created every time I die with an existing bedroll. Should only happen once.
+                    if (!playerData.contains("HolderSpawnX")) {
+                        Druidcraft.LOGGER.debug("Spawn holder created.");
+                        playerData.putInt("HolderSpawnX", mainSpawn.getX());
+                        playerData.putInt("HolderSpawnY", mainSpawn.getY());
+                        playerData.putInt("HolderSpawnZ", mainSpawn.getZ());
+                    }
+                    player.setSpawnPoint(tempPos, true, player.dimension);
+                } else {
+                    Druidcraft.LOGGER.debug("Bedroll not existent.");
+                    if (playerData.contains("HolderSpawnX")) {
+                        Druidcraft.LOGGER.debug("Main spawn restored.");
+                        int holderX = playerData.getInt("HolderSpawnX");
+                        int holderY = playerData.getInt("HolderSpawnY");
+                        int holderZ = playerData.getInt("HolderSpawnZ");
+                        BlockPos holderSpawn = new BlockPos(holderX, holderY, holderZ);
+                        player.setSpawnPoint(holderSpawn, false, player.dimension);
+                        playerData.remove("HolderSpawnX");
+                        playerData.remove("HolderSpawnY");
+                        playerData.remove("HolderSpawnZ");
+                    }
+                    playerData.remove("TempSpawnX");
+                    playerData.remove("TempSpawnY");
+                    playerData.remove("TempSpawnZ");
+                }
             }
         }
-    }
+    }*/
 
-    @SubscribeEvent
-    public void onSpawn(PlayerEvent.PlayerRespawnEvent event) {
-        PlayerEntity player = event.getPlayer();
-        LazyOptional<ITempSpawnCapability> lazyTempSpawn = player.getCapability(TempSpawnProvider.TEMP_SPAWN);
-        ITempSpawnCapability tempSpawn = lazyTempSpawn.orElse(new TempSpawnCapability());
-        BlockPos spawnPos = player.getBedLocation();
-        BlockPos tempSpawnPos = tempSpawn.getTempSpawn();
-        Druidcraft.LOGGER.debug("Real Spawn = " + spawnPos);
-        Druidcraft.LOGGER.debug("Temp Spawn = " + tempSpawnPos);
-/*        if (tempSpawnPos != null) {
-            if ()
-                if (!(player.getEntityWorld().getBlockState(tempSpawnPos).getBlock() instanceof BedrollBlock)) {
-                    tempSpawn.setTempSpawn(null);
-                } else {
-                    player.respawnPlayer();
-                }
-
-        }*/
-    }
+/*    @SubscribeEvent
+    public void onCloned(PlayerEvent.Clone event) {
+        if (!event.getPlayer().world.isRemote()) {
+            CompoundNBT oldData = event.getOriginal().getPersistentData();
+            CompoundNBT newData = event.getPlayer().getPersistentData();
+            if (oldData.contains("TempSpawnX")) {
+                newData.putInt("TempSpawnX", oldData.getInt("TempSpawnX"));
+                newData.putInt("TempSpawnY", oldData.getInt("TempSpawnY"));
+                newData.putInt("TempSpawnZ", oldData.getInt("TempSpawnZ"));
+            }
+            if (oldData.contains("HolderSpawnX")) {
+                newData.putInt("HolderSpawnX", oldData.getInt("HolderSpawnX"));
+                newData.putInt("HolderSpawnY", oldData.getInt("HolderSpawnY"));
+                newData.putInt("HolderSpawnZ", oldData.getInt("HolderSpawnZ"));
+            }
+        }
+    }*/
 }
