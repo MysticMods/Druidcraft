f2204931e89f46d4f4b686d6841824d9f4118ab4
Jon McManus <due@wxwhatever.com>
2020-05-10 19:02:06 +1000

    Fix issue with endless recursionFix issue with endless recursion..

--- src/main/java/com/vulp/druidcraft/blocks/tileentities/CrateTileEntity.java
+++ src/main/java/com/vulp/druidcraft/blocks/tileentities/CrateTileEntity.java
@@ -30,6 +30,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.ArrayList;
@@ -254,7 +255,7 @@ public class CrateTileEntity extends LockableLootTileEntity {
             }
             return this.crateHandler.cast();
         }
-        return super.getCapability(cap);
+        return LazyOptional.empty();
     }
 
     private net.minecraftforge.items.IItemHandlerModifiable createHandler() {
