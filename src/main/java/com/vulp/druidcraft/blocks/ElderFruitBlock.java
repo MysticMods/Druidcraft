688c1588a1cbab0ccf74dcc0e98253c2d943640c
Jon <due@wxwhatever.com>
2020-10-22 10:11:03 +1000

    getItem cannot return null
    Return Items.AIR instead.

--- src/main/java/com/vulp/druidcraft/blocks/ElderFruitBlock.java
+++ src/main/java/com/vulp/druidcraft/blocks/ElderFruitBlock.java
@@ -56,7 +56,7 @@ public class ElderFruitBlock extends DynamicCropBlock implements IGrowable {
 
     @Override
     public ItemStack getItem(IBlockReader worldIn, BlockPos pos, BlockState state) {
-        return null;
+        return Items.AIR;
     }
 
     @Override
